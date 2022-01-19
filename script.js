7516172ea7eae3166113188bc3e1277a5c9b49a8
hari-ls <hari@logicsync.com.au>
2022-01-20 00:04:45 +1030

    updated the HTML, added style overrides and added pseudo code for the script

---
+++ script.js
@@ -0,0 +1,11 @@
+// initialise an empty object for data management
+
+// select the target elements
+
+// initialise required operators and variables
+
+// show today's date on the document
+
+// update the colors for past, present, and future textareas
+
+// listen to click event on save button and update the data in the local storage
